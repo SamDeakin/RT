dd84b6b7fad95fcee88735af3bc81f7f431e34d3
Sam Deakin <rsamdeakin@gmail.com>
2019-12-02 00:14:38 -0500

    Adds validation layers

--- RT1/src/main.cpp
+++ RT1/src/main.cpp
@@ -10,7 +10,7 @@ const char* DESIRED_INSTANCE_EXTENSIONS[] = {
         VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME
 };
 
-std::vector<const char*> get_instance_extensions(vk::Instance& instance, const char** glfwExtensions, const uint32_t glfwExtensionCount) {
+std::vector<const char*> get_instance_extensions(const char** glfwExtensions, const uint32_t glfwExtensionCount) {
     std::vector<vk::ExtensionProperties> extensionProperties = vk::enumerateInstanceExtensionProperties();
 
     std::vector<const char*> extensionNames;
@@ -20,7 +20,7 @@ std::vector<const char*> get_instance_extensions(vk::Instance& instance, const c
 
     std::size_t desiredExtensionCount = sizeof(DESIRED_INSTANCE_EXTENSIONS) / sizeof(const char*);
     // A bitmask initialized to false, to check off extensions as we find them
-    std::vector<bool> foundExtensions(desiredExtensionCount,false);
+    std::vector<bool> foundExtensions(desiredExtensionCount, false);
 
     std::cout << "Supported Instance Extensions:" << std::endl;
     for (auto extension : extensionProperties) {
@@ -49,6 +49,44 @@ std::vector<const char*> get_instance_extensions(vk::Instance& instance, const c
     return extensionNames;
 }
 
+const char* DESIRED_INSTANCE_LAYERS[] = {
+        "VK_LAYER_LUNARG_standard_validation", // This includes a standard set of other layers in an optimal order
+};
+
+std::vector<const char*> get_instance_layers() {
+    std::vector<vk::LayerProperties> layerProperties = vk::enumerateInstanceLayerProperties();
+    std::vector<const char*> layerNames;
+
+    std::size_t desiredLayerCount = sizeof(DESIRED_INSTANCE_LAYERS) / sizeof(const char*);
+    // A bitmask initialized to false, to check off extensions as we find them
+    std::vector<bool> foundLayers(desiredLayerCount, false);
+
+    std::cout << "Supported Instance Layers:" << std::endl;
+    for (auto layer : layerProperties) {
+        std::cout << "    ";
+
+        for (std::size_t i = 0; i < desiredLayerCount; i++) {
+            if (!strcmp(DESIRED_INSTANCE_LAYERS[i], layer.layerName)) {
+                // Push the static name, the other may be destroyed when we are done with the extension list.
+                layerNames.push_back(DESIRED_INSTANCE_LAYERS[i]);
+                foundLayers[i] = true;
+                std::cout << "[Enabled] ";
+            }
+        }
+
+        std::cout << layer.layerName << std::endl;
+    }
+
+    // We don't error check missing layers: they may just not be installed
+    for (std::size_t i = 0; i < desiredLayerCount; i++) {
+        if (!foundLayers[i]) {
+            std::cout << "    [Missing] " << DESIRED_INSTANCE_EXTENSIONS[i] << std::endl;
+        }
+    }
+
+    return layerNames;
+}
+
 void init_vulkan(vk::Instance& instance) {
     if (!glfwInit()) {
         throw std::runtime_error("Failed to initialize glfw!");
@@ -63,35 +101,31 @@ void init_vulkan(vk::Instance& instance) {
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "TestEngine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.apiVersion = VK_API_VERSION_1_1;
 
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<const char*> allExtensions = get_instance_extensions(glfwExtensions, glfwExtensionCount);
 
     vk::InstanceCreateInfo instanceCreateInfo{
         vk::InstanceCreateFlags(),
         &appInfo,
         0,
         nullptr,
-        glfwExtensionCount,
-        glfwExtensions
+        static_cast<uint32_t>(allExtensions.size()),
+        allExtensions.data(),
     };
+
+#ifdef _DEBUG
+    // Since layers generally just add debug info we don't want to add them outside of debug
+    std::vector<const char*> layers = get_instance_layers();
+    instanceCreateInfo.enabledLayerCount = layers.size();
+    instanceCreateInfo.ppEnabledLayerNames = layers.data();
+#endif
 
     vk::Result result = vk::createInstance(&instanceCreateInfo, nullptr, &instance);
     if (result != vk::Result::eSuccess) {
-        throw std::runtime_error("Failed to create Vulkan instance with glfw extensions!");
-    }
-
-    std::vector<const char*> allExtensions = get_instance_extensions(instance, glfwExtensions, glfwExtensionCount);
-
-    instance.destroy();
-
-    // Reinitialize with desired extensions
-    instanceCreateInfo.enabledExtensionCount = allExtensions.size();
-    instanceCreateInfo.ppEnabledExtensionNames = allExtensions.data();
-    result = vk::createInstance(&instanceCreateInfo, nullptr, &instance);
-    if (result != vk::Result::eSuccess) {
-        throw std::runtime_error("Failed to create Vulkan instance with desired extensions!");
+        throw std::runtime_error("Failed to create Vulkan instance!");
     }
 }
 
